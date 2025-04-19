a313522ee43dadb534f5a2a1903fdc8fbf288ef2
inugami-dev64 <karl@karlott.net>
2025-04-19 15:18:14 +0300

    Create a console class

---
+++ utils/console_info.cpp
@@ -0,0 +1,64 @@
+//
+// Created by user on 25/04/19.
+//
+
+#include "console_info.h"
+
+#include <cstdio>
+
+#if defined(_WIN32)
+#define WIN32_LEAN_AND_MEAN
+#define VC_EXTRALEAN
+#include <Windows.h>
+#elif defined(__linux__)
+#include <sys/ioctl.h>
+#include <unistd.h>
+#elif defined(__APPLE__)
+#error "MacOS is not supported lol, get fucked!"
+#endif
+
+namespace ap {
+    Rectangle<int> Console::get_console_dimensions() {
+#if defined(_WIN32)
+        CONSOLE_SCREEN_BUFFER_INFO csbi;
+        int cols, rows;
+
+        GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+        cols = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+        rows = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+
+        return Rectangle<int>{cols, rows};
+#elif defined(__linux__)
+        winsize win{};
+        ioctl(STDOUT_FILENO, TIOCGWINSZ, &win);
+
+        return Rectangle<int>{win.ws_col, win.ws_row};
+    }
+#endif
+
+    void Console::output(const char* row) {
+        printf("%s\n", row);
+    }
+
+    void Console::clear_console() {
+#if defined(_WIN32)
+        COORD topLeft  = { 0, 0 };
+        HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+        CONSOLE_SCREEN_BUFFER_INFO screen;
+        DWORD written;
+
+        GetConsoleScreenBufferInfo(console, &screen);
+        FillConsoleOutputCharacterA(
+            console, ' ', screen.dwSize.X * screen.dwSize.Y, topLeft, &written
+        );
+        FillConsoleOutputAttribute(
+            console, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_BLUE,
+            screen.dwSize.X * screen.dwSize.Y, topLeft, &written
+        );
+        SetConsoleCursorPosition(console, topLeft);
+#elif defined(__linux__)
+        printf("\033[2J\033[H");
+#endif
+    }
+
+} // ap
---
+++ utils/console_info.h
@@ -0,0 +1,41 @@
+//
+// Created by user on 25/04/19.
+//
+
+#ifndef CONSOLE_INFO_H
+#define CONSOLE_INFO_H
+
+namespace ap {
+
+    template <typename T>
+    struct Rectangle {
+        T width;
+        T height;
+    };
+
+    /**
+     * Utility class for doing console specific operations platform-independently
+     */
+    class Console {
+    public:
+        /**
+         * Returns the current console dimensions
+         * @return a Rectangle object containing the current console dimensions
+         */
+        static Rectangle<int> get_console_dimensions();
+
+        /**
+         * Outputs data to stdout
+         * @param row specifies the string to output
+         */
+        static void output(const char* row);
+
+        /**
+         * Clears the console
+         */
+        static void clear_console();
+    };
+
+} // ap
+
+#endif //CONSOLE_INFO_H
