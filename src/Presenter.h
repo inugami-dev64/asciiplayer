2b18c031fcabd329a12880771011b4f5e534906c
inugami-dev64 <karmen@nyaa.ee>
2025-04-28 01:24:12 +0300

    Create a Presenter interface for video/audio

---
+++ src/Presenter.h
@@ -0,0 +1,51 @@
+//
+// Created by user on 25/04/28.
+//
+
+#ifndef PRESENTER_H
+#define PRESENTER_H
+
+#include <thread>
+#include <libavutil/frame.h>
+
+#include "WorkQueue.h"
+
+namespace ap {
+    /**
+     * The interface type for stream presenters
+     */
+    class Presenter {
+    public:
+        Presenter(double _frameRate, unsigned int _sampleRate = 48000) :
+            frameRate(_frameRate),
+            sampleRate(_sampleRate) {}
+        virtual ~Presenter() = default;
+
+        /**
+         * Starts the presenter thread
+         */
+        virtual void present() = 0;
+
+        void setFrameRate(double _frameRate) {
+            frameRate = _frameRate;
+        }
+
+        double getFrameRate() const {
+            return frameRate;
+        }
+    protected:
+        double frameRate;
+        unsigned int sampleRate;
+        std::thread presenter;
+        WorkQueue<AVFrame*> workQueue;
+
+        /**
+         * Transforms given frame into something that can be consumed by the presenter
+         * @param frame
+         */
+        virtual void transform(AVFrame* frame) = 0;
+    };
+
+} // ap
+
+#endif //PRESENTER_H
