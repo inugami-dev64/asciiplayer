634ea3f57ad7ed030de87379d2d56e83ff75b0d7
inugami-dev64 <karmen@nyaa.ee>
2025-04-27 22:15:56 +0300

    Write boilerplate code for Player class

---
+++ src/Player.cpp
@@ -0,0 +1,109 @@
+//
+// Created by user on 25/04/20.
+//
+
+#include "Player.h"
+#include <stdexcept>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+namespace ap {
+    Player::Player(const char *filename, Logger logger) :
+        logger(logger)
+    {
+        logger.log(DEBUG, "Initializing all the containers, codecs and protocols");
+
+        pFormatContext = avformat_alloc_context();
+        if (!pFormatContext)
+            throw runtime_error("Could not allocate memory for format context");
+
+        logger.log(DEBUG, std::string("Attempting to open an input file %s" + std::string(filename)).c_str());
+
+        // Open the file and read its header
+        if (avformat_open_input(&pFormatContext, filename, nullptr, nullptr) != 0)
+            throw std::runtime_error("Could not open input file " + std::string(filename) + ".");
+
+        stringstream ss;
+        // now we have access to some information about the file
+        ss << "format " << pFormatContext->iformat->name <<
+            ", duration " << pFormatContext->duration <<
+            ", bit_rate " << pFormatContext->bit_rate << std::endl;
+        logger.log(DEBUG, ss.str().c_str());
+
+        logger.log(DEBUG, "Finding stream info from format");
+
+        // read packets stream information
+        if (avformat_find_stream_info(pFormatContext, nullptr) < 0)
+            throw std::runtime_error("Could not find stream information");
+
+        // Create codec contexts for video and audio streams
+        _findCodecContext();
+
+        pPacket = av_packet_alloc();
+        if (!pPacket)
+            throw runtime_error("Could not allocate memory for AVPacket");
+    }
+
+    Player::~Player() {
+        avformat_close_input(&pFormatContext);
+        av_packet_free(&pPacket);
+        avformat_free_context(pFormatContext);
+    }
+
+    AVCodecContext* Player::_initializeCodecContext(const AVCodec* pCodec, const AVCodecParameters* pParams) {
+        AVCodecContext* pCodecContext = avcodec_alloc_context3(pCodec);
+        if (!pCodecContext)
+            throw runtime_error("Could not allocate AVCodecContext");
+
+        // Fill the codec context based on the values provided by the codec parameters
+        if (avcodec_parameters_to_context(pCodecContext, pParams) < 0)
+            throw runtime_error("Failed to copy codec parameters to context");
+
+        // Initialize the AVCodecContext to use given AVCodec
+        if (avcodec_open2(pCodecContext, pCodec, nullptr) < 0)
+            throw runtime_error("Could not open codec");
+
+        return pCodecContext;
+    }
+
+    void Player::_findCodecContext() {
+        const AVCodec *pVideoCodec = nullptr;
+        const AVCodecParameters *pVideoCodecParams = nullptr;
+
+        const AVCodec *pAudioCodec = nullptr;
+        const AVCodecParameters *pAudioCodecParams = nullptr;
+
+        for (unsigned int i = 0; i < pFormatContext->nb_streams; i++) {
+            AVCodecParameters *pLocalCodecParams = pFormatContext->streams[i]->codecpar;
+            logger.log(DEBUG , ("AVStream->time_base before open coded "s + to_string(pFormatContext->streams[i]->time_base.num) + "/" + to_string(pFormatContext->streams[i]->time_base.den)).c_str());
+            logger.log(DEBUG, ("AVStream->r_frame_rate before open coded "s + to_string(pFormatContext->streams[i]->r_frame_rate.num) + "/" + to_string(pFormatContext->streams[i]->r_frame_rate.den)).c_str());
+            logger.log(DEBUG, ("AVStream->start_time "s + to_string(pFormatContext->streams[i]->start_time)).c_str());
+            logger.log(DEBUG, ("AVStream->duration "s + to_string(pFormatContext->streams[i]->duration)).c_str());
+
+            logger.log(DEBUG, "finding the proper decoder (CODEC)");
+
+            const AVCodec* pLocalCodec = avcodec_find_decoder(pLocalCodecParams->codec_id);
+            if (pLocalCodec == nullptr)
+                throw runtime_error("Unsupported codec!");
+
+            // we will save the video stream's index, codec parameters
+            if (pLocalCodecParams->codec_type == AVMEDIA_TYPE_VIDEO) {
+                videoStreamIndex = i;
+                pVideoCodec = pLocalCodec;
+                pVideoCodecParams = pLocalCodecParams;
+            } else if (pLocalCodecParams->codec_type == AVMEDIA_TYPE_AUDIO) {
+                audioStreamIndex = i;
+                pAudioCodec = pLocalCodec;
+                pAudioCodecParams = pLocalCodecParams;
+            }
+        }
+
+        if (videoStreamIndex == -1)
+            throw runtime_error("Could not find a video stream!");
+
+        pVideoCodecContext = _initializeCodecContext(pVideoCodec, pVideoCodecParams);
+        pAudioCodecContext = _initializeCodecContext(pAudioCodec, pAudioCodecParams);
+    }
+} // ap
---
+++ src/Player.h
@@ -0,0 +1,42 @@
+//
+// Created by user on 25/04/20.
+//
+
+#ifndef PLAYER_H
+#define PLAYER_H
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
+
+#include "Logger.h"
+
+namespace ap {
+    /**
+     * The main video player class
+     * Just feed the filename and play the video, and it does the magic :D
+     */
+    class Player {
+    public:
+        explicit Player(const char* filename, Logger logger);
+        ~Player();
+
+        void play();
+
+    private:
+        Logger logger;
+        AVFormatContext* pFormatContext = nullptr;
+        AVCodecContext* pVideoCodecContext = nullptr;
+        unsigned int videoStreamIndex = -1;
+        AVCodecContext* pAudioCodecContext = nullptr;
+        unsigned int audioStreamIndex = -1;
+
+        // video rendering specific stuff
+        AVPacket* pPacket = nullptr;
+
+        static AVCodecContext* _initializeCodecContext(const AVCodec* pCodec, const AVCodecParameters* pParams);
+        void _findCodecContext();
+    };
+
+} // ap
+
+#endif //PLAYER_H
